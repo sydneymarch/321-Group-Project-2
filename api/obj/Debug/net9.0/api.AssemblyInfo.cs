5be1f5353300479f96b6b64e01975b7155ab0962
Sydney March <sydneymarch@Sydneys-MacBook-Pro-2.local>
2025-10-10 22:12:08 -0500

    page

--- api/obj/Debug/net9.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net9.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3d795f9da7cce01a86b7b099aaa0636a78ce97c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0ef95f8146ce18cdb6707d3d08c6b2fcfb48507")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
